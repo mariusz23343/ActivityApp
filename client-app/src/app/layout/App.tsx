76df7d5ba385cdcd13986bbcc1be1090db6bf4c8
mariusz23343 <mariusz.byczek99@gmail.com>
2021-11-29 22:30:23 +0100

    Adding axios helper file

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import React, { Fragment, useEffect, useState } from 'react';  
-import axios from 'axios';
 import { Container} from 'semantic-ui-react'
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -12,8 +12,8 @@ function App() {
   const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
-    axios.get<Activity[]>('https://localhost:44319/api/Activities').then(response => {
-      setActivities(response.data);
+    agent.Activities.list().then(response => {
+      setActivities(response);
     })
   }, []);
 
@@ -32,14 +32,14 @@ function App() {
   }
   function handleCreateOrEditActivity(activity: Activity){
     activity.id //patrzymy czy mamy id
-    ? setActivities([...activities.filter(x =>x.id !== activity.id), activity]) //jesli tak to filtrujemy po obecnych aktywnościach, i na aktywnosci o id danym aktualizujemy plus dodajemy poprzednie
-    : setActivities([...activities, {...activity, id: uuid()}]); //jesli nie mamy takiej aktywnosci to dodajemy i dodajemy poprzednie 
+    ? setActivities([...activities.filter(x =>x.id !== activity.id), activity])
+    : setActivities([...activities, {...activity, id: uuid()}]); 
     setEditMode(false);
     setSelectedActivity(activity);
   }
 
   function handleDeleteActivity(id: string){
-    setActivities([...activities.filter(x => x.id !== id)]) // lecimy po wszystkich i zostawiamy tylko te co nie maja id podanego
+    setActivities([...activities.filter(x => x.id !== id)]) 
   }
 
   return (
