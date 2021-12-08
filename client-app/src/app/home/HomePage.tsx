b79d81aa23efef65dcde895eabbeba88edae9d51
mariusz23343 <mariusz.byczek99@gmail.com>
2021-12-08 21:41:38 +0100

    add future components and its styling , clearing app

--- client-app/src/app/home/HomePage.tsx
+++ client-app/src/app/home/HomePage.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
 
 
 export default function HomePage(){
     return(
-        <Container style={{marginTop: '7em'}}>
-            <h1>Home Page</h1>
-            <h3>Go to <Link to='/activities'>Activities</Link></h3>
-        </Container>
+        <Segment inverted textalgn='center' vertucal className='masthead'>
+            <Container text>
+                <Header inverted as='h1'>
+                    <Image size='massive' src='/assets/logo.png'  alt='logo' style={{marginBottom: 12}} />
+                    ActivityApp
+                </Header>
+                <Header>
+                    <Header as='h2' inverted content='Welcome to ActivityApp' />
+                    <Button as={Link} to='/activities' size='huge' inverted>
+                        Open Activities Dashboard
+                    </Button>
+                </Header>
+            </Container>
+        </Segment>
     )
 }
--- client-app/src/app/stores/actvityStore.ts
+++ client-app/src/app/stores/actvityStore.ts
@@ -19,6 +19,16 @@ export default class ActivityStore{
         .sort((a, b) => Date.parse(a.date) - Date.parse(b.date)); //posortowanie po dacie
     }
 
+    get groupedActivities(){
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+            }, {} as {[key: string]: Activity[]} ) 
+        )
+    }
+
     loadActivities = async () => {  // strzalkowa funkcja bo takiej nie trzeba bindowac do klasy
         this.loadingInitial = true;
         try{
