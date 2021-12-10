1dacbe72e594d197a78375aea0b1e4db0454fdb9
mariusz23343 <mariusz.byczek99@gmail.com>
2021-12-10 17:50:02 +0100

    addung not found view, redirection to this view in specific situations, adding tostify events

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,5 +1,7 @@
+import { history } from './../../index';
 import { Activity } from './../models/activity';
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from 'react-toastify';
 
 
 const sleep = (delay: number) => {
@@ -11,14 +13,26 @@ const sleep = (delay: number) => {
 axios.defaults.baseURL = 'https://localhost:44319/api';
 
 axios.interceptors.response.use(async response =>{
-    try {
+
         await sleep(1000);
         return response;
-    } catch (error) {
-        console.log(error);
-        return await Promise.reject(error);
-    }
-})
+    },    (error: AxiosError) => {
+       const{data, status} = error.response!;
+       switch(status){
+           case 400: 
+                toast.error('bad request');
+                break;
+           case 401:
+                toast.error('unauthorised');
+                break;
+           case 404:
+                history.push('/not-found')
+               break;
+           case 500: 
+                toast.error('server error');
+                break;              
+       }
+    })
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,12 +3,13 @@ import { Container } from 'semantic-ui-react'
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
-import { Route, useLocation } from 'react-router';
+import { Route, Switch, useLocation } from 'react-router';
 import HomePage from '../home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import TestErrors from '../../features/activities/Errors/TestError';
 import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/activities/Errors/notFound';
 
 function App() {
 
@@ -24,10 +25,13 @@ function App() {
           <>
              <NavBar />
             <Container style={{marginTop: '4.5em'}}>
-              <Route exact path="/activities" component={ActivityDashboard} />
-              <Route path="/activities/:id" component={ActivityDetails} />
-              <Route key={location.key} path={["/createActivity", '/manage/:id']} component={ActivityForm} />
-              <Route path='/errors' component={TestErrors} />
+              <Switch>
+                <Route exact path="/activities" component={ActivityDashboard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route key={location.key} path={["/createActivity", '/manage/:id']} component={ActivityForm} />
+                <Route path='/errors' component={TestErrors} />
+                <Route component={NotFound} />
+              </Switch> 
             </Container>
           </>
         )}
