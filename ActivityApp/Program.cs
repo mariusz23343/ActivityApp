8bf7c8550cb2c6dbcbf2b7e4bb3774a0dc8a6d5c
mariusz23343 <mariusz.byczek99@gmail.com>
2021-12-16 10:12:02 +0100

    add test users seed

--- ActivityApp/Program.cs
+++ ActivityApp/Program.cs
@@ -1,4 +1,6 @@
+using Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,8 +25,9 @@ namespace ActivityApp
             try
             {
                 var context = services.GetRequiredService<DataContext>();
+                var usermanager = services.GetRequiredService<UserManager<AppUser>>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedData(context);
+                await Seed.SeedData(context, usermanager);
             }
             catch(Exception ex)
             {
