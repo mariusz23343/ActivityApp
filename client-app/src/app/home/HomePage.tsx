5fdf7517e51ababd91304920f84becea25b8751a
mariusz23343 <mariusz.byczek99@gmail.com>
2021-12-16 23:46:24 +0100

    updating homepage and navbar, adding logout method

--- client-app/src/app/home/HomePage.tsx
+++ client-app/src/app/home/HomePage.tsx
@@ -1,9 +1,14 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
+import { useStore } from "../stores/store";
 
 
-export default function HomePage(){
+export default observer(function HomePage(){
+
+    const {userStore} = useStore();
+
     return(
         <Segment inverted textalgn='center' vertical className='masthead'>
             <Container text>
@@ -11,13 +16,20 @@ export default function HomePage(){
                     <Image size='massive' src='/assets/logo.png'  alt='logo' style={{marginBottom: 12}} />
                     ActivityApp
                 </Header>
-                <Header>
-                    <Header as='h2' inverted content='Welcome to ActivityApp' />
+                {userStore.isLoggedIn ? (
+                    <>
+                        <Header as='h2' inverted content='Welcome to ActivityApp' />
+                        <Button as={Link} to='/activities' size='huge' inverted>
+                        Go To Activities!
+                        </Button>
+                    </>
+                   
+                ) : (
                     <Button as={Link} to='/login' size='huge' inverted>
-                        Login
+                    Login
                     </Button>
-                </Header>
+                )}
             </Container>
         </Segment>
     )
-}
+})
