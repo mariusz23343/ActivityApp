9a7ad2f7abda03af5df55d9e1072fff443065d0a
mariusz23343 <mariusz.byczek99@gmail.com>
2021-12-16 22:07:36 +0100

    setting token upon login to local storage, add login and logout method to userStore, set user (with token) to userStore

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -4,11 +4,23 @@ import { ServerError } from './../models/ServerError';
 
 export default class CommonStore{
     error: ServerError | null = null;
+    token: string | null = null;
+    appLoaded = false;
+
     constructor() {
         makeAutoObservable(this);
     }
 
     setServerError = (error: ServerError) => {
         this.error = error;
+    }
+
+    setToken = (token: string | null) => {
+        if(token) window.localStorage.setItem('jwt', token);
+        this.token = token;
+    }
+
+    setAppLoaded = () => {
+        this.appLoaded = true;
     }
 }
--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,6 +1,9 @@
+import { history } from './../../index';
+import { runInAction } from 'mobx';
 import { User, UserFormValues } from './../models/user';
 import { makeAutoObservable } from 'mobx';
 import agent from '../api/agent';
+import { store } from './store';
 export default class UserStore {
     user: User | null = null;
 
@@ -14,10 +17,21 @@ export default class UserStore {
 
     login= async (creds: UserFormValues) => {
         try{
-            const user = await agent.Account.login(creds)
-            console.log(user);
+            const user: User = await agent.Account.login(creds)
+            store.commonStore.setToken(user.token);
+            runInAction(() =>{
+                this.user = user;
+            });
+            history.push('/activities');
         } catch (error) {
             throw error;
         }
+    }
+
+    logout = () => {
+        store.commonStore.setToken(null);
+        window.localStorage.removeItem('jwt');
+        this.user = null;
+        history.push('/')
     }
 }
