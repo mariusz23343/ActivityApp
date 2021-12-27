6bd9fa51905968c28f917f7a5ae46f9c13d2615a
Mariusz Byczek <mariusz.byczek99@gmail.com>
2021-12-27 18:15:59 +0100

    UserPanel

--- ActivityApp/DTOs/UserDto.cs
+++ ActivityApp/DTOs/UserDto.cs
@@ -6,6 +6,6 @@ namespace API.DTOs
         public string DisplayName { get; set; }
         public string Token { get; set; }
         public string Username { get; set; }
-        public string Image { get; set; }
+        public string? Image { get; set; }
     }
 }
