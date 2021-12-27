a12626362ed46cecef04c4b601c3585c65d9e9af
Mariusz Byczek <mariusz.byczek99@gmail.com>
2021-12-27 14:02:57 +0100

    Adding delete photo path

--- ActivityApp/Controllers/PhotosController.cs
+++ ActivityApp/Controllers/PhotosController.cs
@@ -14,5 +14,11 @@ namespace API.Controllers
         {
             return HandleResult(await mediator.Send(command));
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            return HandleResult(await mediator.Send(new Delete.Command { Id = id }));
+        }
     }
 }
