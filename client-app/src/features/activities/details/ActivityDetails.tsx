5e2cded5a390e43c73af586c98be3fcf8fa3f366
mariusz23343 <mariusz.byczek99@gmail.com>
2022-01-12 20:08:32 +0100

    adding small changest to singalR backend code, adjusting code, adding signalR client side code

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,5 +1,5 @@
 import { observer } from "mobx-react-lite";
-import React, { useEffect } from "react"; 
+import React, { useEffect } from "react";
 import { useParams } from "react-router";
 import { Grid } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
@@ -9,28 +9,31 @@ import ActivityDetailedHeader from "./ActivityDetailedHeader";
 import ActivityDetailedInfo from "./ActivityDetailedInfo";
 import ActivityDetailedSidebar from "./ActivityDetailedSidebar";
 
+export default observer(function ActivityDetails() {
+  const { activityStore } = useStore();
+  const {
+    selectedActivity: activity,
+    loadActivity,
+    loadingInitial,
+    clearSelectedActivity,
+  } = activityStore;
+  const { id } = useParams<{ id: string }>();
+  useEffect(() => {
+    if (id) loadActivity(id);
+    return () => clearSelectedActivity();
+  }, [id, loadActivity, clearSelectedActivity]);
 
-
-export default observer(function ActivityDetails(){
-    
-    const {activityStore} = useStore();
-    const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
-    const {id} = useParams<{id: string}>();
-    useEffect(() => {
-        if(id) loadActivity(id);
-    }, [id, loadActivity])
-
-    if(loadingInitial || !activity) return <LoadingComponent />;
-    return (
-      <Grid>
-          <Grid.Column width={10}>
-              <ActivityDetailedHeader activity={activity} />
-              <ActivityDetailedInfo activity={activity} />
-              <ActivityDetailedChat />
-          </Grid.Column>
-          <Grid.Column width={6}>
-              <ActivityDetailedSidebar activity={activity}/>
-          </Grid.Column>
-      </Grid>
-    )
-})
+  if (loadingInitial || !activity) return <LoadingComponent />;
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityDetailedHeader activity={activity} />
+        <ActivityDetailedInfo activity={activity} />
+        <ActivityDetailedChat activityId={activity.id} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <ActivityDetailedSidebar activity={activity} />
+      </Grid.Column>
+    </Grid>
+  );
+});
