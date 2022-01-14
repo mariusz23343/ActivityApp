7a3d2e3c4b52ce9bc1a018a296bcec7247daca1c
mariusz23343 <mariusz.byczek99@gmail.com>
2022-01-14 14:26:13 +0100

    adding follow/unfollow feature

---
+++ ActivityApp/Controllers/FollowController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Followers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class FollowController : BaseApiController
+    {
+        [HttpPost("{username}")]
+        public async Task<IActionResult> Follow(string username)
+        {
+            return HandleResult(await mediator.Send(new FollowToggle.Command { TargetUsername = username }));
+        }
+    }
+}
